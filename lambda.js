f058937b5601ade5dec783952e7836441e472b55
Daniel Long <danlong13579@gmail.com>
2020-10-22 21:09:06 -0400

    cleaned up event json & started Doc -runs in node-

--- lambda.js
+++ lambda.js
@@ -1,31 +1,36 @@
-const pramEvent = require('./event.json')
-
 exports.handler = async (event) => {
-    // TODO implement
     let res = {}
     
     //GET_METHOD
     if (event.httpMethod === 'GET') {
+
+        //null check on api Params
         if(event.queryStringParameters != null){
             
+            //TODO
+            //check for case of the keys as lowercase 'speed' fails request
+            
+            //set params to var so we can parce them into numbers
             let dataPointSpeed = parseInt(event["queryStringParameters"]["Speed"]) 
             let dataPointDistance = parseInt(event["queryStringParameters"]["Distance"])
-            
+
             if(dataPointSpeed >= 1 && dataPointDistance >= 1){
                 let dataTime = dataPointDistance / dataPointSpeed
                 let dataSeconds = dataTime * 3600 | 0
+
+                let jsonBody = JSON.stringify({
+                    message: "Request aproved",
+                    speed: dataPointSpeed,
+                    distance: dataPointDistance,
+                    TimeToHex: `${dataSeconds}s`,
+                    EventLog: event
+                })
+
                 res = {
                     statusCode: 200,
-                    body: JSON.stringify({
-                        message: "Request aproved",
-                        speed: dataPointSpeed,
-                        distance: dataPointDistance,
-                        TimeToHex: `${dataSeconds}s`,
-                        eventsize: event
-                    })
+                    body: jsonBody
                 }
-                 console.log(dataSeconds)
-                
+                 console.log(jsonBody)
                 
             }else if (dataPointSpeed < 1){
                 res = {
