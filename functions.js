c35e1286fac5d592dff3e2219403e10ae9d9ecef
Daniel Long <danlong13579@gmail.com>
2020-11-03 16:34:58 -0500

    restructured for better error handling

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-function MatchString(text, event ) {
+let MatchString = (text, event) => {
     let jsonData = JSON.stringify(event)
     let regexTest = new RegExp(text, "i")
     let res = regexTest.exec(jsonData)
@@ -16,4 +16,8 @@ let HexDist = (cords) => {
     return (Math.abs(cords[1] - cords[3]) + Math.abs(cords[1] + cords[0] - cords[3] - cords[2]) + Math.abs(cords[0] - cords[2])) / 2
 }
 
-module.exports = { MatchString, MatchCords, HexDist }
+let TimeToHex = (time) => {
+    return time[0]
+}
+
+module.exports = { MatchString, MatchCords, HexDist, TimeToHex }
--- lambda.js
+++ lambda.js
@@ -2,70 +2,65 @@ const F = require('./functions')
 
 exports.handler = async (event) => {
     let res = {}
+    let jsonBody = ""
 
+    let dataPointSpeed = parseInt(event["queryStringParameters"][F.MatchString("speed", event["queryStringParameters"])])
+    let dataPointDistance = parseInt(event["queryStringParameters"][F.MatchString("distance", event["queryStringParameters"])])
+
+    let cordsData = F.MatchCords(event["queryStringParameters"][F.MatchString("cords", event["queryStringParameters"])])
+    let cords = cordsData.map(num => parseInt(num, 10))
+    let distance = F.HexDist(cords)
     //GET_METHOD
-    if (event.httpMethod === 'GET') {
-        
-        //null check on api Params
-        if(event.queryStringParameters != null){
-            
-            let dataPointSpeed = parseInt(event["queryStringParameters"][F.MatchString("speed", event["queryStringParameters"])])
-            let dataPointDistance = parseInt(event["queryStringParameters"][F.MatchString("distance", event["queryStringParameters"])])
-
-            let cordsData = F.MatchCords(event["queryStringParameters"][F.MatchString("cords", event["queryStringParameters"])])
-            let cords = cordsData.map(num => parseInt(num, 10))
-            let distance = F.HexDist(cords)
-
-            if(dataPointSpeed >= 1 && dataPointDistance >= 1){
-
-                // starborn convets its time to seconds and rounds down
-                let dataTime = dataPointDistance / dataPointSpeed
-                let dataSeconds = dataTime * 3600 | 0 // <-- faster math.floor
-
-
-
-                let jsonBody = JSON.stringify({
-                    message: "Request aproved",
-                    speed: dataPointSpeed,
-                    distance: dataPointDistance,
-                    TimeToHex: `${dataSeconds / 3600 | 0}:${dataSeconds % 3600 / 60 | 0}:${dataSeconds % 60 | 0} HH:MM:SS`
-                })
-                
+    if (event.httpMethod != null) {
+        if(event.httpMethod === "GET"){
+            if(event.queryStringParameters != null){
                 res = {
                     statusCode: 200,
-                    body: jsonBody
+                    body: JSON.stringify({
+                        "message": "Request accepted",
+                        "eventParams": event.queryStringParameters,
+                        "eventParamsArray": event.multiValueQueryStringParameters
+                    })
                 }
-
-                console.log(distance)
-                console.log(jsonBody)
-            }else if (dataPointSpeed < 1){
+                return res
+            }
+            else {
                 res = {
                     statusCode: 400,
                     body: JSON.stringify({
-                        message: "Invalid Speed param",
-                        speedParam: dataPointSpeed
+                        "message": "No params or malformed syntax",
+                        "paramsList": {
+                            "speed" : "22",
+                            "distance": "44",
+                            "defaultParams" : {
+                                "hasCords" : "false",
+                                "hasArrivalTime" : "false"
+                            }
+                        }
                     })
                 }
+                return res
             }
+
         }
-        else
-        {
+        else {
             res = {
-                statusCode: 400,
-                    body: JSON.stringify({
-                        message: "Missing param Speed & Distance"
-                    })
+                statusCode: 405,
+                body: JSON.stringify({
+                    "message": "Only GET methods are allowed please see ReadMe file for a list of params and calls",
+                    "url": "https://github.com/Danlong13579/starborn-api-calc",
+                    "httpMethod": event.httpMethod
+                })
             }
+            return res
         }
-        
-        return res
-    }
-    
-    // POST_METHOD
-    if (event.httpMethod === 'POST') {
+    } 
+    else {
         res = {
-            statusCode: 200,
-            body: JSON.stringify(event.queryStringParameters)
+            statusCode: 400,
+            body: JSON.stringify({
+                "message": "No http method used"
+            })
         }
         return res
     }
